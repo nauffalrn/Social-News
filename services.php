3ad09909c45bb55bb0d32ea623696eb5099f828d
Laksana Muhammad Agung <agunglefroeg@gmail.com>
2024-12-23 12:30:12 +0700

    update services

--- services.php
+++ services.php
@@ -1,7 +1,40 @@
 <?php include 'partials/header.php'; ?>
 
-    <section class="empty__page">
-        <h1>Services Page</h1>
-    </section>
+<section class="services">
+    <div class="container services__container">
+        <h1>Our Services</h1>
+        <p>Selamat datang di Social News, sumber utama Anda untuk semua berita dan informasi terkini.
+            Kami menawarkan berbagai layanan yang dirancang untuk memenuhi kebutuhan berita Anda dan memberikan pengalaman yang memuaskan.
+            Dengan menggunakan Social News, Anda dapat menemukan berita terbaru dan terkini dari berbagai kategori, berinteraksi dengan komunitas yang bersemangat, dan tetap terhubung dengan dunia di sekitar Anda.
+        </p>
 
-    <?php include 'partials/footer.php'; ?>
+        <div class="services__features">
+            <article class="service">
+                <h3>Liputan Berita yang Komprehensif</h3>
+                <p>
+                    Tetap terhubung dengan berita terbaru dan terkini dari berbagai kategori, termasuk teknologi, bisnis, hiburan, dan banyak lagi.
+                </p>
+            </article>
+            <article class="service">
+                <h3>Tampilan Antarmuka yang Ramah Pengguna</h3>
+                <p>
+                Navigasi yang mudah melalui situs web kami dengan desain menarik dan responsif yang dioptimalkan untuk semua perangkat.
+                </p>
+            </article>
+            <article class="service">
+                <h3>Interaksi Antar Pengguna</h3>
+                <p>
+                Berinteraksi dengan komunitas yang aktif melalui komentar, diskusi, dan berbagi artikel yang menarik.
+                </p>
+            </article>
+            <article class="service">
+                <h3>Konten Berkualitas Tinggi</h3>
+                <p>
+                    Kami menyajikan konten berita yang terpercaya dan akurat untuk memastikan informasi yang kami berikan selalu berkualitas.
+                </p>
+            </article>
+        </div>
+    </div>
+</section>
+
+<?php include 'partials/footer.php'; ?>
