168497270400e98c12efe4ffc07f0c2927407657
Laksana Muhammad Agung <agunglefroeg@gmail.com>
2024-12-30 22:00:15 +0700

    pls

--- admin/config/database.php
+++ admin/config/database.php
@@ -3,7 +3,8 @@ require_once __DIR__ . '/../../config/constants.php';
 
 // Connect to the database
 $connection = mysqli_init();
-$connection->ssl_set(NULL, NULL, '../../DigiCertGlobalRootCA.crt.pem', NULL, NULL);
+$cert_path = __DIR__ . '/../../DigiCertGlobalRootCA.crt.pem';
+$connection->ssl_set(NULL, NULL, $cert_path, NULL, NULL);
 $connection->real_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT, NULL, MYSQLI_CLIENT_SSL);
 
 if(!$connection){
