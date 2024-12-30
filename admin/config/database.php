6c91d13569b5b6bfa424760c9970692c6db724fd
Laksana Muhammad Agung <agunglefroeg@gmail.com>
2024-12-30 21:38:42 +0700

    pls bisa aku udah cape

--- admin/config/database.php
+++ admin/config/database.php
@@ -1,7 +1,10 @@
 <?php
 require_once __DIR__ . '/../../config/constants.php';
 
-$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+// Connect to the database
+$connection = mysqli_init();
+$connection->ssl_set(NULL, NULL, 'DigiCertGlobalRootCA.crt.pem', NULL, NULL);
+$connection->real_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT, NULL, MYSQLI_CLIENT_SSL);
 
 if(!$connection){
     die("Connection Failed: " . mysqli_connect_error());
