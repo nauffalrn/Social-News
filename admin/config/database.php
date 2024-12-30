ddb2388e8f3c2baa4574b9b627f57d77d3db8a95
Laksana Muhammad Agung <agunglefroeg@gmail.com>
2024-12-30 23:12:55 +0700

    constants.php

--- admin/config/database.php
+++ admin/config/database.php
@@ -1,6 +1,6 @@
 <?php
-// filepath: /home/site/wwwroot/config/database.php
-require_once __DIR__ . '/constants.php';
+// filepath: /home/site/wwwroot/admin/config/database.php
+require_once __DIR__ . '/../../config/constants.php';
 
 // Connect to the database
 $connection = mysqli_init();
