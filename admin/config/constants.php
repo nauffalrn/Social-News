04d9ea5286248187e739b038f07e2e698e3e614c
Laksana Muhammad Agung <agunglefroeg@gmail.com>
2025-01-01 16:34:30 +0700

    last try

---
+++ admin/config/constants.php
@@ -0,0 +1,15 @@
+<?php
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+define('ROOT_URL', 'https://socialnews.azurewebsites.net/');
+define('DB_HOST','socialnews.mysql.database.azure.com');
+define('DB_PORT', 3306);
+define('DB_USER','socialnews');
+define('DB_PASS','Admin123');
+define('SSL_MODE','require');
+define('DB_NAME','socialnews');
+
+?>
